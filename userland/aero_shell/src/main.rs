64734a8b9ad01c6bf02e1d54a6ace34bed60ed7f
YusufKhan-human <54283715+YusufKhan-gamedev@users.noreply.github.com>
2022-01-06 23:25:33 -0800

    Uwu gpl

--- userland/aero_shell/src/main.rs
+++ userland/aero_shell/src/main.rs
@@ -1,21 +1,22 @@
 /*
 * Copyright (C) 2021-2022 The Aero Project Developers.
- *Copyright (C) 2022 The Uwux Project Developers.
- * This file is part of The Aero Project abd The Uwux project.
  *
- * Uwux is free software: you can redistribute it and/or modify
+ * This file is part of The Aero Project.
+ *
+ * Aero is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Uwux is distributed in the hope that it will be useful,
+ * Aero is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Uwux. If not, see <https://www.gnu.org/licenses/>.
+ * along with Aero. If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 use aero_syscall::*;
 
