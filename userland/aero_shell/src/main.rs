a67c9fc4ed6f798eac5f34be04133ece38cb4dc3
YusufKhan-human <54283715+YusufKhan-gamedev@users.noreply.github.com>
2022-01-06 23:00:46 -0800

    Uwu mowe

--- userland/aero_shell/src/main.rs
+++ userland/aero_shell/src/main.rs
@@ -1,20 +1,20 @@
 /*
 * Copyright (C) 2021-2022 The Aero Project Developers.
+ *Copyright (C) 2022 The Uwux Project Developers.
+ * This file is part of The Aero Project abd The Uwux project.
  *
- * This file is part of The Aero Project.
- *
- * Aero is free software: you can redistribute it and/or modify
+ * Uwux is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Aero is distributed in the hope that it will be useful,
+ * Uwux is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Aero. If not, see <https://www.gnu.org/licenses/>.
+ * along with Uwux. If not, see <https://www.gnu.org/licenses/>.
  */
 
 use aero_syscall::*;
@@ -384,7 +384,7 @@ fn uwufetch() -> Result<(), AeroSyscallError> {
             println!("{}", "-".repeat(username.len() + hostname.len() + 1));
         } else if i == 3 {
             print_prefix("OS");
-            println!("Aero");
+            println!("Uwux");
         } else if i == 4 {
             let tty_fd = sys_open("/dev/tty", OpenFlags::O_RDONLY)?;
 
